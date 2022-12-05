6d5a46abfa59ffa85b90a662bd68bd2a21bf761e
Quoc Bao <nguyenquocbaost.2002@gmail.com>
2022-12-06 01:27:33 +0700

    test

--- src/main/java/com/CurrencyExchangeApplication.java
+++ src/main/java/com/CurrencyExchangeApplication.java
@@ -7,6 +7,8 @@ import java.util.Optional;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -22,35 +24,36 @@ import com.entity.Currency;
 @SpringBootApplication()
 @Controller
 //@RequestMapping("exchange")
-@ComponentScan(basePackages= {"com"})
-public class CurrencyExchangeApplication {
-	
-BusinessCurrency businessCurrency = new BusinessCurrency();
-	
+@ComponentScan(basePackages = { "com" })
+public class CurrencyExchangeApplication extends SpringBootServletInitializer {
+
+	BusinessCurrency businessCurrency = new BusinessCurrency();
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CurrencyExchangeApplication.class);
+	}
+
 	@RequestMapping("/")
-	public String search(ModelMap model, 
-			@RequestParam(name = "fromCode", required = false) String fromCode,
-			@RequestParam("toCode") Optional<String> toCode,
-			@RequestParam("amount") Optional<String> amount)
-	throws IOException, ParseException {
+	public String search(ModelMap model, @RequestParam(name = "fromCode", required = false) String fromCode,
+			@RequestParam("toCode") Optional<String> toCode, @RequestParam("amount") Optional<String> amount)
+			throws IOException, ParseException {
 		String amountDouble = amount.orElse("1.0");
 		String toCodeString = toCode.orElse("VND");
 		String result = businessCurrency.getRateWithAmount(fromCode, toCodeString, Double.valueOf(amountDouble));
-		
-		
+
 		List<Currency> currencies = businessCurrency.getAllCurrencies();
-		String json = businessCurrency.getTop30RatesWithAmountJson(fromCode, toCodeString, Double.valueOf(amountDouble));
-		
-		
-		
+		String json = businessCurrency.getTop30RatesWithAmountJson(fromCode, toCodeString,
+				Double.valueOf(amountDouble));
+
 		model.addAttribute("result", result);
 		model.addAttribute("json", json);
 		model.addAttribute("symbols", currencies);
-		
+
 		model.addAttribute("fromCode", fromCode);
 		model.addAttribute("toCode", toCodeString);
 		model.addAttribute("amount", amountDouble);
-		
+
 //		ModelAndView mav = new ModelAndView("index");
 //		
 //		mav.addObject("result", result);
@@ -62,7 +65,7 @@ BusinessCurrency businessCurrency = new BusinessCurrency();
 
 		return "index";
 	}
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyExchangeApplication.class, args);
 	}
