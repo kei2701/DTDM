86db9e653964e0f48ab087a6b3f02d2afc2316e5
Quoc Bao <nguyenquocbaost.2002@gmail.com>
2022-12-05 20:21:24 +0700

    test

--- src/main/java/com/CurrencyExchangeApplication.java
+++ src/main/java/com/CurrencyExchangeApplication.java
@@ -2,8 +2,12 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.controller.ExchangeController;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses=ExchangeController.class)
 public class CurrencyExchangeApplication {
 
 	public static void main(String[] args) {
